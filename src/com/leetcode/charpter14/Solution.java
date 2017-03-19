40177cc1eaaf90aca5c6544dac412074433c4ff3
yy <448953616@qq.com>
2017-03-19 16:35:11 +0800

    first

--- src/com/leetcode/charpter14/Solution.java
+++ src/com/leetcode/charpter14/Solution.java
@@ -8,43 +8,61 @@ package com.leetcode.charpter14;
 
 public class Solution {
     public static String longestCommonPrefix(String[] strs) {
-        //排除空指针，空数组
-        if(strs!=null&&strs.length!=0) {
-            String s=null;
-            boolean flag = true;
-            int index =0;
+        /**
+         //排除空指针，空数组
+         if(strs!=null&&strs.length!=0) {
+         String s=null;
+         boolean flag = true;
+         int index =0;
 
-            //计算最短字符串长度
-            int minLen = strs[0].length();
-            for (int i=1;i<strs.length;i++){
-                if (strs[i].length()<minLen) minLen = strs[i].length();
-            }
+         //计算最短字符串长度
+         int minLen = strs[0].length();
+         for (int i=1;i<strs.length;i++){
+         if (strs[i].length()<minLen) minLen = strs[i].length();
+         }
 
-            //按字符索引开始循环，从0开始
-            for (int i=0;i<minLen&&flag;i++){
+         //按字符索引开始循环，从0开始
+         for (int i=0;i<minLen&&flag;i++){
 
-                char fisrtStringChar = strs[0].charAt(i);
+         char fisrtStringChar = strs[0].charAt(i);
 
-                //按字符串数组索引开始循环，如果当前字符串的第j个字符与第一个字符串的第j个不符，flag设置为TRUE；并记录此时的j值index=j-1;
-                for (int j =0;j<strs.length&&flag;j++){
-                    if (strs[j].charAt(i)!=fisrtStringChar){
-                        flag = false;
-                        break;
-                    }
-                }
+         //按字符串数组索引开始循环，如果当前字符串的第j个字符与第一个字符串的第j个不符，flag设置为TRUE；并记录此时的j值index=j-1;
+         for (int j =0;j<strs.length&&flag;j++){
+         if (strs[j].charAt(i)!=fisrtStringChar){
+         flag = false;
+         break;
+         }
+         }
 
-                if (flag){
-                    index +=1;
-                }
+         if (flag){
+         index +=1;
+         }
 
-            }
+         }
 
-            s = strs[0].substring(0,index);
+         s = strs[0].substring(0,index);
 
-            return s;
-        }else{
-            return "";
+         return s;
+         }else{
+         return "";
+         }
+         */
+
+
+        /**
+         * 优化方法，取第一个字符串为结果字符串，对之后的每一个字符串调用indexOf方法，不断取pre.substring(0,pre.length()-1);
+         * 直到在该字符串中匹配到该结果字符串，继续循环下一个字符串
+         * PS：indexOf方法对于查找不到的字符串会返回-1；如果找到了返回的位置数应该是0；
+         */
+        if(strs == null || strs.length == 0)    return "";
+        String pre = strs[0];
+        int i = 1;
+        while(i < strs.length){
+            while(strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0,pre.length()-1);
+            i++;
         }
+        return pre;
 
     }
 
--- src/com/leetcode/charpter9/Solution.java
+++ src/com/leetcode/charpter9/Solution.java
@@ -6,23 +6,40 @@ package com.leetcode.charpter9;
 public class Solution {
 
     public static boolean isPalindrome(int x) {
-        // 负数不是回访数字
-        if (x < 0) {
-            return false;
+        /**
+         *
+         *  负数不是回访数字
+         if (x < 0) {
+         return false;
+         }
+
+         // 数字逆转后的值，为了不使用溢出采用long
+         long reverse = 0;
+         int tmp = x;
+
+         // 求逆转后的值
+         while (tmp != 0) {
+         reverse = reverse * 10 + tmp % 10;
+         tmp /= 10;
+         }
+
+         // 判断是否是回文数字
+         return x == reverse;
+
+         */
+
+
+        /**
+         * 优化方法，首先判断输入是否为负数或者是否以0结尾，返回FALSE；
+         * rev是否大于前半段，直到小于活等于时结束，判断rev是否等于前半段或者除10后是否相等，相等则是会文
+         */
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int rev = 0;
+        while (x>rev){
+            rev = rev*10 + x%10;
+            x = x/10;
         }
-
-        // 数字逆转后的值，为了不使用溢出采用long
-        long reverse = 0;
-        int tmp = x;
-
-        // 求逆转后的值
-        while (tmp != 0) {
-            reverse = reverse * 10 + tmp % 10;
-            tmp /= 10;
-        }
-
-        // 判断是否是回文数字
-        return x == reverse;
+        return (x==rev || x==rev/10);
 
     }
 
